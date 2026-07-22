9446260e2cdef232de4743e4b4b3c1595c347288
vamsikrish98 <vamsikrisshna06@gmail.com>
2026-07-22 06:08:20 +0530

    Update stg_google_ads.sql

--- models/staging/stg_google_ads.sql
+++ models/staging/stg_google_ads.sql
@@ -1,1 +1,9 @@
-
+SELECT
+    campaign_id,
+    LOWER(TRIM(campaign_name)) AS campaign_name,
+    CAST(cost AS NUMERIC) AS spend,
+    CAST(impressions AS INT64) AS impressions,
+    CAST(clicks AS INT64) AS clicks,
+    PARSE_DATE('%m/%d/%Y', date) AS campaign_date,
+    'google' AS channel
+FROM {{ ref('google_ads') }}
