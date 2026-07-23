4e6cb01e074521a39683299da3b32f55629c036e
vamsikrish98 <vamsikrisshna06@gmail.com>
2026-07-23 10:39:14 +0530

    Add SQL query for low performing campaigns

--- analyses/low_performing_campaigns.sql
+++ analyses/low_performing_campaigns.sql
@@ -1,1 +1,18 @@
-
+SELECT
+    campaign_id,
+    campaign_name,
+    channel,
+    total_spend,
+    total_revenue,
+    total_conversions,
+    roas,
+    cac,
+    roi,
+    conversion_rate
+FROM {{ ref('fct_campaign_performance') }}
+WHERE roas < 1
+   OR roi < 0
+   OR total_conversions = 0
+ORDER BY
+    total_spend DESC,
+    roas ASC
